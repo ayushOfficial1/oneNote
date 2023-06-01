8c34c7540a82db6c0b02abb636e2af9ecd7beeac
Ayush Gupta <19ec1038@mitsgwl.ac.in>
2023-06-01 19:43:29 +0530

    added PrivateRoute

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import "./style.scss";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import NoteState from "./context/notes/NoteState.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 function App() {
+  const PrivateRoute = ({ children }) => {
+    const isAuthenticated = Cookies.get("authtoken");
+
+    if (!isAuthenticated) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
   return (
     <>
       <NoteState>
         <BrowserRouter basename="oneNote">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
             <Route path="/login" element={<Login />} />
-            <Route path="/reg" element={<Register />} />           
+            <Route path="/reg" element={<Register />} />
           </Routes>
         </BrowserRouter>
       </NoteState>
